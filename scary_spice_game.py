2293e3ff95033437cabe172b18064823fa9d1d4e
Faniia Prazdnikova <funnear@gmail.com>
2025-02-20 17:44:45 +0100

    reverted some unnecessary changes and added a new game script

---
+++ scary_spice_game.py
@@ -0,0 +1,60 @@
+from vikingsClasses import *
+
+in_any_bad_situation = "Something is badly wrong, game over."
+
+
+def recruit_soldiers(soldier_type: str):
+    count_soldiers = int(input(f"How many {soldier_type} soldiers will fight: "))
+    
+    for i in range(count_soldiers):
+        if soldier_type == "Viking":
+            name = input(f"What is the name of {soldier_type} {i+1}: ")
+
+        health = int(input(f"Health points of {soldier_type} {i+1}: "))
+        strength = int(input(f"Strength points of {soldier_type} {i+1}: "))
+
+        if soldier_type == "Viking":
+            war.vikingArmy.append(Viking(name, health, strength))
+            print(f"Viking {name} with HP {health} and SP {strength} has joined the force.")
+        elif soldier_type == "Saxon":
+            war.saxonArmy.append(Saxon(health, strength))
+            print(f"Some Saxon with HP {health} and SP {strength} has joined the force.")
+        else:
+            raise ValueError(in_any_bad_situation)
+    
+
+    # this code doesn't work because there are no get methods for reading properties
+    print(f"{soldier_type}s army is ready to fight! We have:")
+    if soldier_type == "Viking":
+        for viking in war.vikingArmy:
+            print(f"- {viking.name} with {viking.health} HP and {viking.strength} SP.")
+    elif soldier_type == "Saxon":
+        for saxon in war.saxonArmy:
+            print(f"- a soldier with {saxon.health} HP and {saxon.strength} SP.")
+    print(f"All armed and dangerous.")
+
+def create_armies():
+    recruit_soldiers(soldier_type="Viking")
+    recruit_soldiers(soldier_type="Saxon")
+    user_interaction()
+
+def user_interaction():
+    if not (war.vikingArmy and war.saxonArmy):
+        print("Armies need warriors.")
+        create_armies()
+    
+    while war.showStatus() == "Vikings and Saxons are still in the thick of battle.":
+        user_action = int(input(" Press 1 for Vikings attack\n Press 2 for Saxons attack\n: "))
+        if user_action == 1:
+            print("Vikings attack!")
+            attack_result = war.vikingAttack()
+            print(attack_result)
+        if user_action == 2:
+            print("Saxons attack!")
+            attack_result = war.saxonAttack()
+            print(attack_result)
+        
+if __name__ == '__main__':
+    print("Vikings and Saxons are in the state of war!")
+    war = War()
+    user_interaction()
